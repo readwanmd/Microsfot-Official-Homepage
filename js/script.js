5a08ac6c6de74a3134185d0fa526195c1db34443
readwanmd <readwanmd@gmail.com>
2021-04-02 01:34:39 +0600

    Scroll Btn Done

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
+// selection
 const menuBtn = document.querySelector('.menu-btn');
 const mainMenu = document.querySelector('.main-menu');
+const scrollToTopBtn = document.querySelector('.scroll-to-top-btn');
 
+// Nav Toggle
 menuBtn.addEventListener('click', () => {
 	mainMenu.classList.toggle('show');
 });
 
-const scrollToTopBtn = document.querySelector('.scroll-to-top-btn');
+// Scroll Button Hide/Show
+window.addEventListener('scroll', () => {
+	const scrollY = window.scrollY;
+	console.log(scrollY);
 
+	if (Math.round(scrollY) <= 250) {
+		scrollToTopBtn.style.display = 'none';
+	} else scrollToTopBtn.style.display = 'block';
+});
+
+// Scroll to top
 scrollToTopBtn.addEventListener('click', () => {
 	window.scrollTo({
 		top: 0,
